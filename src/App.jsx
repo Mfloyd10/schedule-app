7eaf9ac2025d66c77f84dcc6cd4993bbe7daedc6
darkwvy <mfloyd874@gmail.com>
2026-05-04 20:10:30 -0400

    Initial app set up. Cleaned vite template.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1>This is my Schedule App</h1>
+    </main>
+  )
+}
+
+export default App
