5875ea6a58ca10bda807f0d231c391314d47ed49
darkwvy <mfloyd874@gmail.com>
2026-05-05 04:55:17 -0400

    feat: scaffold dashboard layout and navigation

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../supabaseClient'
+import pizzaIcon from "../assets/pizzaIcon.png"
+import {HiLogout} from "react-icons/hi";
+import "./Dashboard.css"
 
 export default function Dashboard() {
     const [session, setSession] = useState(null)
+    const [user, setUser] = useState(null)
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -16,7 +20,45 @@ export default function Dashboard() {
         })
     }, [])
 
+    const handleLogout = async () => {
+        await supabase.auth.signOut()
+        navigate('/login')
+    }
+
     if (!session) return null
 
-    return <div>Dashboard</div>
+    return (
+        <div className="dashboardWrapper">
+            <nav className="topBar">
+                <div className="topBarLeft">
+                    <img className="dashboardLogo" src={pizzaIcon} alt="pizzaIcon"/>
+                    <div className="leftWrapper">
+                        <span className="title">A Slice of Time</span>
+                        <span className="currentUser">{session.user.email}</span>
+                    </div>
+                </div>
+                <div className="topBarRight">
+                    <div className="logoutWrapper">
+                        <button
+                            className="logoutButton"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </button>
+                        <span>
+                            <HiLogout/>
+                        </span>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="dashboardBody">
+                <div className="sideNav">
+                    <h1>This is the side bar</h1>
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
